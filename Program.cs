f74f95ad8879c80a01a1db51ff62116b2cf03c99
r-paulo-santos <Paulo.Ricardo_santos@outlook.com>
2020-02-28 20:28:21 -0300

    foi adicionada os nomes das cidades do litoral de São Paulo

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace cidades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mongaguá");
+            Console.WriteLine("Itanhaém");
+            Console.WriteLine("Peruibe"); 
+        }
+    }
+}
